3f8dffa86d91723a7109307559d2bec7df1c14cf
BankaiXHunterr <bankaixhunter@gmail.com>
2023-09-06 17:02:05 +0530

    updated clover with frontend

--- app.py
+++ app.py
@@ -16,21 +16,14 @@ import numpy as np
 import openpyxl
 import xlrd
 from pathlib import Path
+from static.module.utils import numerical_variable
 
 # Create the Flask app instance
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/'
 
 # Define the numerical variables and their ranges
-numerical_variable = {'Elevation': (1900, 3900), 'Aspect': (0, 400),
-                      'Slope': (0, 100), 'Horizontal_Distance_To_Hydrology': (0, 1400),
-                      'Vertical_Distance_To_Hydrology': (-200, 600),
-                      'Horizontal_Distance_To_Roadways': (0, 7100),
-                      'Hillshade_9am': (0, 300), 'Hillshade_Noon': (0, 300),
-                      'Hillshade_3pm': (0, 300),
-                      'Horizontal_Distance_To_Fire_Points': (0, 7200),
-                      'Wilderness_Area_Type': (0, 3),
-                      'Soil_Type': (0, 39)}
+numerical_variable = numerical_variable
 
 
 def data_processor(data):
---
+++ static/module/utils.py
@@ -0,0 +1,9 @@
+numerical_variable = {'Elevation': (1900, 3900), 'Aspect': (0, 400),
+                      'Slope': (0, 100), 'Horizontal_Distance_To_Hydrology': (0, 1400),
+                      'Vertical_Distance_To_Hydrology': (-200, 600),
+                      'Horizontal_Distance_To_Roadways': (0, 7100),
+                      'Hillshade_9am': (0, 300), 'Hillshade_Noon': (0, 300),
+                      'Hillshade_3pm': (0, 300),
+                      'Horizontal_Distance_To_Fire_Points': (0, 7200),
+                      'Wilderness_Area_Type': (0, 3),
+                      'Soil_Type': (0, 39)}
